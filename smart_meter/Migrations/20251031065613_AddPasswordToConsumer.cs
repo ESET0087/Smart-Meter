4654fe6ae8e2ca8ae58aab56ab16fea1ecc914b9
Akashesya <Akash.agrahari@esyasoft.com>
2025-11-04 17:07:05 +0530

    changes

---
+++ smart_meter/Migrations/20251031065613_AddPasswordToConsumer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace smart_meter.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordToConsumer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "passwordhash",
+                table: "consumer",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "passwordhash",
+                table: "consumer");
+        }
+    }
+}
